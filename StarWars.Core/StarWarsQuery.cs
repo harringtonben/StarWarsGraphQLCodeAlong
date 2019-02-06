2836e9a36bb9d43e1bc455cb39fc3ca3e552b475
ben harrington <harringtonben@icloud.com>
2019-02-06 09:05:37 -0600

    finished spinning up basic api

--- StarWars.Core/StarWarsQuery.cs
+++ StarWars.Core/StarWarsQuery.cs
@@ -1,7 +1,15 @@
+using GraphQL.Types;
+
 namespace StarWars.Core
 {
-    public class StarWarsQuery
+    public class StarWarsQuery : ObjectGraphType
     {
-        
+        public StarWarsQuery()
+        {
+            Field<DroidType>(
+                "hero",
+                resolve: context => new Droid {Id = 1, Name = "R2D2"}
+            );
+        }
     }
 }
